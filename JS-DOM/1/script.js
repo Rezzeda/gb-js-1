a9ae2c261ed4e6d78e1bde43a928754716a859c8
Reseda <pro@pro.local>
2024-03-02 14:25:12 +0400

    fixed JS-DOM hw1

--- JS-DOM/1/script.js
+++ JS-DOM/1/script.js
@@ -39,18 +39,18 @@
 // // console.log(scheduleData);
 
 //имитируем строку с сервера (начальные данные)
-    initialSchedule = '[{"id":1,"name":"Йога","time":"10:00 - 11:00","maxParticipants":15,"currentParticipants":8},{"id":2,"name":"Пилатес","time":"11:30 - 12:30","maxParticipants":10,"currentParticipants":5},{"id":3,"name":"Кроссфит","time":"13:00 - 14:00","maxParticipants":20,"currentParticipants":15},{"id":4,"name":"Танцы","time":"14:30 - 15:30","maxParticipants":12,"currentParticipants":10},{"id":5,"name":"Бокс","time":"16:00 - 17:00","maxParticipants":8,"currentParticipants":6}]';
+const initialSchedule = '[{"id":1,"name":"Йога","time":"10:00 - 11:00","maxParticipants":15,"currentParticipants":8},{"id":2,"name":"Пилатес","time":"11:30 - 12:30","maxParticipants":10,"currentParticipants":5},{"id":3,"name":"Кроссфит","time":"13:00 - 14:00","maxParticipants":20,"currentParticipants":15},{"id":4,"name":"Танцы","time":"14:30 - 15:30","maxParticipants":12,"currentParticipants":10},{"id":5,"name":"Бокс","time":"16:00 - 17:00","maxParticipants":8,"currentParticipants":6}]';
 
 //ключ для хранения данных в localStorage
-    const lsKey = "lesson";
-    const userKey = "user";
+const lsKey = "lesson";
+const userKey = "user";
 
-    if (!localStorage.getItem(lsKey)) {
-        localStorage.setItem(lsKey, initialSchedule);
-    }
+if (!localStorage.getItem(lsKey)) {
+    localStorage.setItem(lsKey, initialSchedule);
+}
     
-    const scheduleTable = document.querySelector(".scheduleBody");
-    const lessons = JSON.parse(localStorage.getItem(lsKey));
+const scheduleTable = document.querySelector(".scheduleBody");
+const lessons = JSON.parse(localStorage.getItem(lsKey));
 
 // Проверяем, записан ли пользователь на занятие при загрузке страницы
 const userData = JSON.parse(localStorage.getItem(userKey)) || [];
